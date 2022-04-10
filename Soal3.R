96b3c1a2147ab33ea875b8040bf5fe6a3e957acc
Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2022-04-10 16:49:10 +0700

    Commit Soal3

---
+++ Soal3.R
@@ -0,0 +1,24 @@
+# Diketahui data dari sebuah tempat bersalin di rumah sakit tertentu menunjukkan rata-rata historis
+# 4,5 bayi lahir di rumah sakit ini setiap hari. (gunakan Distribusi Poisson)
+  Lambda <- 4.5
+ 
+# Soal 3A: Berapa peluang bahwa 6 bayi akan lahir di rumah sakit ini besok?
+  X <- 6
+  poiss_dist_i <- dpois(X,Lambda)
+ 
+# Soal 3B: Simulasikan dan buatlah histogram kelahiran 6 bayi akan lahir di rumah sakit ini selama setahun (n = 365)
+  N <- 365
+  poiss_dist_ii <- rpois(N,Lambda)
+  hist(poiss_dist_ii,main = "Grafik histogram distribusi poisson kelahiran 6 bayi selama 1 tahun" )
+
+# Soal 3C: bandingkan hasil poin a dan b , Apa kesimpulan yang bisa didapatkan?
+  mean(poiss_dist_ii == 6)
+ 
+  # Pada poin a nilainya statis, yaitu 0.12812. Pada poin b nilainya dinamis,
+  # tetapi mendekati nilai A. Sehingga dapat ditarik kesimpulan bahwa
+  # meskipun nilai b bernilai dinamis, tetapi nilai poin b selalu mendekati poin a
+ 
+# SOal 3D: Nilai Rataan (μ) dan Varian (σ²) dari Distribusi Poisson.
+  mean <- Lambda
+  variance <- mean
+ 
